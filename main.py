f6ba4034bf7be1830ea2ca70887b123c14ac2de0
AritraBanik08 <banikaritra4@gmail.com>
2024-04-05 02:01:45 +0530

    005

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory="templates/main")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"id": id}
+    )
